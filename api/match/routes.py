68de2f9349d77bc2017d27cd13dc3f8fd282beb7
Alex Ramirez <alexanderjamesramirez@gmail.com>
2022-03-24 23:39:28 -0700

    update

--- api/match/routes.py
+++ api/match/routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
 from app import db
-from api.match.models import Match, MatchPhase, MatchResult, MatchRound
+from api.match.models import Match, MatchPhase, MatchResult, MatchRound, MatchHero
 from api.map.models import Map
+from api.hero.models import Hero
 from flask_login import current_user
 
 match = Blueprint('match', __name__, url_prefix='/match')
@@ -12,6 +13,9 @@ def add_match():
     map_played = Map.query.filter(Map.id==1).first_or_404()
     initial_map_round = MatchRound()
     new_match = Match(user_id=current_user.id, ranked_flag=True, map_played=map_played, rounds=[initial_map_round], result=MatchResult.VICTORY)
+    a = MatchHero()
+    a.hero = Hero.query.filter(Hero.id==1).first_or_404()
+    new_match.heroes_played.append(a)
     print(new_match)
     db.session.add(new_match)
     db.session.commit()
