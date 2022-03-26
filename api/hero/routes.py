282f43074d6d8e641904e6aa1173fea87265fbe4
David <garzadavid26@yahoo.com>
2022-03-25 22:55:41 -0700

    refactoring

--- api/hero/routes.py
+++ api/hero/routes.py
@@ -1,18 +1,7 @@
 from flask import make_response, jsonify, redirect, url_for, Blueprint, flash
-from .utils import add_all_ow_heroes
 from api.hero.models import Hero
-from app import db
 
 hero = Blueprint('hero', __name__, url_prefix='/hero')
-
-@hero.post('/add_heroes')
-def add_heroes() :
-    add_all_ow_heroes()
-
-    if not db.query(Hero).first() :
-        flash('Heroes not added')
-    else :
-        return redirect((url_for('view_heroes')))
 
 @hero.get('/view_heroes')
 def view_heroes() :
