641aef1ba5fca79afe2a3086cc0664f59df8caa1
alex <alexanderjamesramirez@gmail.com>
2022-03-26 19:10:37 -0700

    implemented hero roles when creating heroes and added dataclass attr to hero and hero_roles

--- api/hero/models.py
+++ api/hero/models.py
@@ -7,12 +7,20 @@ from app import db
 
 @dataclass
 class HeroRole(db.Model):
+    id : int
+    title : str
+    
     __tablename__ = "ow_hero_role"
     id = db.Column(db.Integer, primary_key=True, autoincrement= True)
     title = db.Column(db.String(25), nullable=False, unique=True)
 
 @dataclass
 class Hero(db.Model):
+    id : int
+    name : str
+    hero_role_id: int
+    hero_role: HeroRole
+
     __tablename__ = "ow_hero"
     id = db.Column(db.Integer, primary_key=True, autoincrement= True)
     name = db.Column(db.String(25))
