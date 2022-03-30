b5b3dcaa32d379338a38f519a98e278e7026bb04
Alex Ramirez <alexanderjamesramirez@gmail.com>
2022-03-30 01:23:42 -0700

    roles are now displated in dashboard

--- api/match/models.py
+++ api/match/models.py
@@ -5,6 +5,8 @@ from app import db
 from api.hero.models import Hero
 from api.map.models import Map
 from typing import List
+from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
+from sqlalchemy import select, func
 
 
 class MatchResult(enum.Enum):
@@ -29,7 +31,9 @@ class Match(db.Model):
     heroes_played : List[Hero]
     ranked_flag : bool
     result : MatchResult
+    result_formatted : str
 
+    roles : List[str]
 
     __tablename__ = "ow_match"
     id = db.Column(db.Integer, primary_key=True, autoincrement= True)
@@ -43,6 +47,54 @@ class Match(db.Model):
     heroes_played = db.relationship("MatchHero")
 
     result = db.Column(Enum(MatchResult))
+
+    @property
+    def roles(self):
+        match_roles = []
+        if self.tank_hero_count > 0:
+            match_roles.append("Tank")
+        if self.damage_hero_count > 0:
+            match_roles.append("Damage")
+        if self.support_hero_count > 0:
+            match_roles.append("Support")
+        return match_roles
+
+    @property
+    def result_formatted(self):
+        return self.result.value
+
+    @hybrid_property
+    def tank_hero_count(self):
+        return sum([1 for p in self.heroes_played if p.hero.hero_role.title == "Tank"])   # @note: use when non-dynamic relationship
+
+    @tank_hero_count.expression
+    def tank_hero_count(cls):
+        return (select([func.count(MatchHero.hero_id)]).
+                where(MatchHero.match_id == cls.id).
+                label("tank_hero_count")
+                )
+
+    @hybrid_property
+    def damage_hero_count(self):
+        return sum([1 for p in self.heroes_played if p.hero.hero_role.title == "Damage"])   # @note: use when non-dynamic relationship
+
+    @damage_hero_count.expression
+    def damage_hero_count(cls):
+        return (select([func.count(MatchHero.hero_id)]).
+                where(MatchHero.match_id == cls.id).
+                label("damage_hero_count")
+                )
+
+    @hybrid_property
+    def support_hero_count(self):
+        return sum([1 for p in self.heroes_played if p.hero.hero_role.title == "Support"])   # @note: use when non-dynamic relationship
+
+    @support_hero_count.expression
+    def support_hero_count(cls):
+        return (select([func.count(MatchHero.hero_id)]).
+                where(MatchHero.match_id == cls.id).
+                label("support_hero_count")
+                )
 
 @dataclass
 class MatchRound(db.Model):
