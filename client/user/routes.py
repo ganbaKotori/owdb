d7f072b08a887b656e9ca2105b7856d9a7dfa8c9
Alex Ramirez <alexanderjamesramirez@gmail.com>
2022-08-03 12:51:27 -0700

    initial commit for user profile feature

--- client/user/routes.py
+++ client/user/routes.py
@@ -2,3 +2,10 @@ from flask import render_template, redirect, url_for, Blueprint
 from flask_login import login_required, current_user
 
 user = Blueprint('user', __name__)
+
+@user.get('/<string:username>')
+@login_required
+def get_user_profile_page(username):
+    return render_template('user/user_profile.html', user=user)
+
+
